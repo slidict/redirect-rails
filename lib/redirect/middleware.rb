8bf9cccabe3c7a389620d38917744b08f95531af
yusuke abe <yusuke@slidict.io>
2024-07-13 14:33:38 +0900

    Added the function the redirecting the hosts.

--- lib/redirect/middleware.rb
+++ lib/redirect/middleware.rb
@@ -8,18 +8,28 @@ module Redirect
 
     def call(env)
       @request = Rack::Request.new(env)
-      if url = redirect_url(@request.path)
+      if url = redirect_url(@request.path) || url = redirect_host(@request.host)
         redirect_to(url)
       else
         @app.call(env)
       end
     end
 
+    private
+
     def redirect_url(request_path)
       return unless Redirect::Rails.paths
 
       Redirect::Rails.paths.map do |path, redirect_path|
         return ERB.new(redirect_path).result(binding) if Regexp.new(path).match(request_path)
+      end.first
+    end
+
+    def redirect_host(request_host)
+      return unless Redirect::Rails.hosts
+
+      Redirect::Rails.hosts.map do |host, redirect_url|
+        return ERB.new(redirect_url).result(binding) if Regexp.new(host).match(request_url)
       end.first
     end
 
--- lib/redirect/rails/version.rb
+++ lib/redirect/rails/version.rb
@@ -2,6 +2,6 @@
 
 module Redirect
   module Rails
-    VERSION = "0.2.2"
+    VERSION = "0.3.0"
   end
 end
